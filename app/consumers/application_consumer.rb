11b0a43cf2d78788048b8981b58bcc4fdea0c67d
Roman Blanco <rblanco@redhat.com>
2024-10-22 12:39:27 +0000

    feat: install karafka library
    bundle add karafka --version ">= 2.4.0"
    bundle exec karafka install

--- app/consumers/application_consumer.rb
+++ app/consumers/application_consumer.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
-require 'racecar/consumer'
-
-# Parent class for all Racecar consumers, contains general logic
-class ApplicationConsumer < Racecar::Consumer
-  def process(message)
-    @msg_value = JSON.parse(message.value)
-    logger.info "Received message, enqueueing: #{@msg_value}"
-  end
-
-  protected
-
-  def logger
-    Rails.logger
-  end
+# Application consumer from which all Karafka consumers should inherit
+# You can rename it if it would conflict with your current code base (in case you're integrating
+# Karafka with other frameworks)
+class ApplicationConsumer < Karafka::BaseConsumer
 end
