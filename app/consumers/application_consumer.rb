a7f672402512e12052d4742b6871d23dc8d1b2e4
Roman Blanco <rblanco@redhat.com>
2024-10-23 13:54:18 +0000

    feat: change code to correspond with Karafka

--- app/consumers/application_consumer.rb
+++ app/consumers/application_consumer.rb
@@ -4,4 +4,21 @@
 # You can rename it if it would conflict with your current code base (in case you're integrating
 # Karafka with other frameworks)
 class ApplicationConsumer < Karafka::BaseConsumer
+  attr_reader :msg_value
+  def consume
+    messages.each do |message|
+      @msg_value = message
+      consume_one
+
+      mark_as_consumed(message)
+    end
+  end
+
+  def consume_one
+    logger.info "Received message, enqueueing: #{@msg_value}"
+  end
+
+  def logger
+    Rails.logger
+  end
 end
